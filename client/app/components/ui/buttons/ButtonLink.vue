d107edf78b311b74c93ed2f7d2ea20ab3a6a5a4c
Kowenjko <ortoswt@gmail.com>
2025-12-21 16:36:42 +0200

    fix bug button link

--- client/app/components/ui/buttons/ButtonLink.vue
+++ client/app/components/ui/buttons/ButtonLink.vue
@@ -1,29 +1,32 @@
 <script setup lang="ts">
+import type { RouteLocationRaw } from 'vue-router'
 const {
-  text = "",
+  text = '',
   badge,
   to,
 } = defineProps<{
-  to: string | { name: string };
-  text?: string;
-  badge?: number;
-}>();
+  to: RouteLocationRaw
+  text?: string
+  badge?: number
+}>()
 </script>
 
 <template>
   <nuxt-link
-    :to
+    :to="to"
     class="relative flex items-center space-x-2 font-medium text-gray-700 transition-colors hover:text-black"
   >
     <slot />
 
     <span v-if="text">{{ text }}</span>
 
-    <span
-      v-if="badge && badge > 0"
-      class="absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-black text-xs font-bold text-white"
-    >
-      {{ badge }}
-    </span>
+    <ClientOnly>
+      <span
+        v-if="badge && badge > 0"
+        class="absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-black text-xs font-bold text-white"
+      >
+        {{ badge }}
+      </span>
+    </ClientOnly>
   </nuxt-link>
 </template>
